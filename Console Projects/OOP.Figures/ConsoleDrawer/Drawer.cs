eac5d3765c3b20fc876da99a0c058c28eea6dfbe
FixSad <ost.odino4ka@bk.ru>
2024-10-03 01:19:36 +0300

    class library ConsoleDrawer
    interface & drawer

---
+++ Console Projects/OOP.Figures/ConsoleDrawer/Drawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDrawer
+{
+    public class Drawer : IDrawer
+    {
+        private const char SYMBOL = '.';
+
+        public void Draw(int x, int y, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+            Console.Write(SYMBOL);
+        }
+
+        public void Erase(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(' ');
+        }
+    }
+}
---
+++ Console Projects/OOP.Figures/ConsoleDrawer/IDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDrawer
+{
+    public interface IDrawer
+    {
+        void Draw(int x, int y, ConsoleColor color = ConsoleColor.White);
+        void Erase(int x, int y);
+    }
+}
